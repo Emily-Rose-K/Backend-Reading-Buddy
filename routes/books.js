afafbfc8a88fb61156fb0244946963c8c0395aa8
Eliott Davidson <ulthos@gmail.com>
2020-09-03 21:49:33 -0700

    added large seeder file and Modified books route file to allow for db seeding

--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,9 @@ const readerExperience = require("../models/ReaderExperience");
 const Book = require("../models/Book");
 const User = require("../models/User");
 
+//const SEEDFILE = require('../seed2.js'); 
+const BIG_OL_LIST = require('../seed2.js');
+
 router.get("/:api_id", (req,res) => {
     Book.find({title: req.query.title, author: req.query.author}) // we want reviews for all editions of the book, not just the one specified by req.params.api_id
         .populate("readerExperiences")
@@ -22,6 +25,38 @@ router.get("/:api_id", (req,res) => {
         })
         .catch(err => {
             res.send({error: `Error in books controller show route: ${err}`});
+        })
+})
+
+// only use for seeding db
+router.post("/", (req, res) => {
+    // format list so we can insert it with a single Mongoose create() call.
+    BIG_OL_LIST_FOR_MONGOOSE = BIG_OL_LIST.map(book => {
+        // I think this sort of isbn number always has 10 digits, 
+        // and our seed file sometimes leaves out initial zeros, 
+        // so we need to put them in in order for openlibrary API to work
+        while ( book[2].length < 10 ) {
+            book[2] = `0${book[2]}`;
+        }
+        return {title: book[0], author: book[1], api_id: book[2]}
+    })
+    console.log(`🔴🔴🔴🔴🔴 ${JSON.stringify(BIG_OL_LIST_FOR_MONGOOSE[0])}`);
+    Book.find({title: BIG_OL_LIST_FOR_MONGOOSE[0].title})
+        .then(foundBook => {
+            if (foundBook.title) {
+                console.log(`Seeding aborted - the database may be already seeded: ${foundBook}`)
+            } else {
+                Book.create(BIG_OL_LIST_FOR_MONGOOSE)
+                    .then(result => {
+                        res.send(`Database seeded.  Db response: ${JSON.stringify(result)}`);
+                    })
+                    .catch(err => {
+                        res.send(`error seeding database: ${err}`)
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(`error determining whether database was previously seeded: ${err}`)
         })
 })
 
